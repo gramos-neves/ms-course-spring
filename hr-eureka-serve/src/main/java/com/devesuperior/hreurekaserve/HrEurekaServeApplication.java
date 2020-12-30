4812d824f587b605d1b0d30222f587779b25b046
xxjamisxx <guineves.ramos@gmail.com>
2020-12-30 14:38:03 -0300

    Atualizaçao

---
+++ hr-eureka-serve/src/main/java/com/devesuperior/hreurekaserve/HrEurekaServeApplication.java
@@ -0,0 +1,15 @@
+package com.devesuperior.hreurekaserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class HrEurekaServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServeApplication.class, args);
+	}
+
+}
---
+++ hr-eureka-serve/src/test/java/com/devesuperior/hreurekaserve/HrEurekaServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devesuperior.hreurekaserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
